03563caa7fd03b6cdb83ded6028aae90f703eadc
Seol_JY <wlsdud5654@gmail.com>
2022-08-08 02:45:34 +0900

    [FIX]: Port

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,6 +4,7 @@ const dbo = require("../src/config/db")
 const logger = require("../src/config/logger")
 const PORT = process.env.PORT || 80; 
 
+
 app.listen(PORT, () => {
     logger.info("Server Start... ");
     logger.info(`Server listening on port ${PORT}`);
